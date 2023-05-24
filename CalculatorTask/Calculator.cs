e8f50a24d71699fb8ca6615102e683cec2a6b9a1
Maha Alyemeni <52765342+MahaSaud99@users.noreply.github.com>
2023-05-24 16:35:11 +0300

    Add files via upload

---
+++ CalculatorTask/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTask
+{
+    internal class Calculator
+    {
+
+        public double sum(double x , double y)
+        {
+            return x + y;
+        }
+
+        public double sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double div(double x, double y)
+        {
+            return x / y;
+        }
+
+        public double multi(double x, double y)
+        {
+            return x * y;
+        }
+
+
+    }
+}
---
+++ CalculatorTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Enter first number :");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Sum
+            double sumResult = calculator.sum(num1, num2);
+            Console.WriteLine(sumResult);
+
+            // Sub
+            double subResult = calculator.sub(num1, num2);
+            Console.WriteLine(subResult);
+
+            // multi
+            double multiResult = calculator.multi(num1, num2);
+            Console.WriteLine(multiResult);
+
+            // div 
+            double divResult = calculator.div(num1, num2);
+            Console.WriteLine(divResult);
+        }
+    }
+}
